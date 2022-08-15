7072718423e2d7e3b1a9cba0cce99797114630ec
HASSAN OLOWOFELA <hasbah2009@gmail.com>
2022-08-15 00:19:09 -0400

    Testing level

---
+++ Intro/Node-js/index.js
@@ -0,0 +1,10 @@
+var superheroes = require("superheroes");
+
+var supervillains = require("supervillains");
+
+var mysuperHero = superheroes.random("");
+
+var mysuperVillain = supervillains.random("");
+
+console.log(mysuperHero);
+console.log(mysuperVillain);
